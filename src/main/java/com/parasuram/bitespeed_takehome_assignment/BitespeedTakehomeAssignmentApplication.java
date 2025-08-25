5cb16b8d04a7f24d0f8a0fd58870447167ec6e79
psr1720 <parasuram.reddy18@gmail.com>
2025-08-25 14:45:25 +0530

    Initial commit

---
+++ src/main/java/com/parasuram/bitespeed_takehome_assignment/BitespeedTakehomeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.parasuram.bitespeed_takehome_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitespeedTakehomeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitespeedTakehomeAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parasuram/bitespeed_takehome_assignment/BitespeedTakehomeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parasuram.bitespeed_takehome_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitespeedTakehomeAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
