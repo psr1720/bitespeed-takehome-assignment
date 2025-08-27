8c75475bdf7713bb67febb887c984394678624ae
psr1720 <parasuram.reddy18@gmail.com>
2025-08-27 14:30:05 +0530

    Added repository functions

---
+++ src/main/java/com/parasuram/bitespeed_takehome_assignment/repository/ContactRepository.java
@@ -0,0 +1,18 @@
+package com.parasuram.bitespeed_takehome_assignment.repository;
+
+import com.parasuram.bitespeed_takehome_assignment.entity.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
+    @Query(value = "SELECT * FROM Contacts WHERE (email = :email OR phone_number = :phoneNumber) AND link_precedence = 'primary'", nativeQuery = true)
+    List<Contact> findPrimaryContactsByEmailOrPhone(@Param("email") String email, @Param("phoneNumber") String phoneNumber);
+
+    @Query(value = "SELECT * FROM Contacts WHERE (email = :email OR phone_number = :phoneNumber) AND link_precedence = 'secondary'", nativeQuery = true)
+    List<Contact> findSecondaryContactsByEmailOrPhone(@Param("email") String email, @Param("phoneNumber") String phoneNumber);
+}
